e86dcd63fa18f62c1be421c7aac4a17339dbccdc
Andreas Heider <heidera@in.tum.de>
2012-06-09 18:59:16 +0200

    Initial zsh and tmux configuration

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# if screen-256color is not available on a system:
+# infocmp screen-256color > /tmp/screen-256color.terminfo (already done)
+# tic screen-256color.terminfo
+
+cd $(dirname $0)
+for dotfile in .?*
+do
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.oh-my-zsh' ]
+    then
+        echo "ln -Fis $PWD/$dotfile $HOME"
+        ln -Fis "$PWD/$dotfile" $HOME
+    fi
+done
+
+echo "ln -Fis \"$PWD/.oh-my-zsh/themes/ah.zsh-theme\" $HOME/.oh-my-zsh/themes"
+ln -Fis "$PWD/.oh-my-zsh/themes/ah.zsh-theme" $HOME/.oh-my-zsh/themes
